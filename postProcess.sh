5ca867520f97ae15c45ced41dfec92a6fce79186
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2024-02-16 20:10:10 -0600

    updates all around

--- postProcess.sh
+++ postProcess.sh
@@ -1,16 +1,18 @@
 #bin/bash
-#SBATCH --nodes=1 --ntasks=1
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=16 --mem=32G 
 #SBATCH --time=1:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=14G
-# don't need GPUs for this lol
-##SBATCH --gres=gpu:2
-# BASEDIR="PLACEHOLDER"
-# to execute this script from somewhere else, 
-# if [[ -z "$SLURMD_NODENAME"]]; then
+
+# Wrote this because I didn't want to spend any more time manually running FFmpeg commands
+# also planning on using this software for a long time, so the better I make the tooling, the easier life gets
+
+# recursively goes through /bin/export and for every subfolder makes an mp4
+# then moves that folder to a videoOutput folder
+
+# I might need to consider using FFmpeg to compress the videos at some point, but meh
 
 
 # VARIABLES FOR SETUP
+# this one's for testing locally
 # BASEDIR="$PWD"
 BASEDIR="$HOME/ambrosic/FluidX3D"
 
@@ -32,21 +34,23 @@ cd $INPUTFOLDER
 find . -maxdepth 1 -mindepth 1 -type d -printf '%P\n'
 printf "GENERATING MP4 FILES \n$DIVIDER\n"
 for D in $(find . -maxdepth 1 -mindepth 1 -type d -printf '%P\n'); do
+    echo "$DIVIDER"
     echo "starting ${D}"
     cd "${INPUTFOLDER}/${D}"
     # ls
     ffmpeg -hide_banner -loglevel error -framerate 30 -pattern_type glob -i "*.png" -c:v libx264 -pix_fmt yuv420p "${D}.mp4"
     echo "${D}.mp4 complete, moving file to output directory"
     # echo "moving ${D}"
-    mv "$INPUTFOLDER/{D}/{D}.mp4" "$OUTPUTFOLDER/${D}.mp4"
+    mv "$INPUTFOLDER/${D}/${D}.mp4" "$OUTPUTFOLDER/${D}.mp4"
     echo "file moved"
-    echo "$DIVIDER"
 done
-
-# old parallel
-# find . -maxdepth 1 -mindepth 1 -type d -printf '%P\n' | parallel 'echo "starting {1}" && cd {1} && ffmpeg -hide_banner -loglevel error -framerate 30 -pattern_type glob -i "*.png" -c:v libx264 -pix_fmt yuv420p {1}.mp4 && echo "{1}.mp4 complete"'
+echo "$DIVIDER"
 # go back to base CWD
 cd $BASEDIR
+
+# rename export folder so it's out of the way for the next job.
+# theoretically I could do this at the end of the CFD job?
+mv "${INPUTFOLDER}" "${INPUTFOLDER}-${DATE}"
 # make moving happen in its own step
 # man am I thankful for stackOverflow
 # printf "$DIVIDER\nMOVING MP4 FILES\n"
