405ed26d0921be7a5c6f153da15c5720fbd44d59
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2024-02-16 19:40:45 -0600

    split post-processing for supercomputing & local runs

--- postProcess.sh
+++ postProcess.sh
@@ -1,42 +1,45 @@
-#!/bin/bash
+#bin/bash
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=14G
 # don't need GPUs for this lol
 ##SBATCH --gres=gpu:2
-BASEDIR="PLACEHOLDER"
+# BASEDIR="PLACEHOLDER"
 # to execute this script from somewhere else, 
-if [[ -z "$SLURMD_NODENAME"]]; then
-    BASEDIR="$PWD"
-else
-    BASEDIR="$HOME/ambrosic/FluidX3D"
-fi
+# if [[ -z "$SLURMD_NODENAME"]]; then
+
+
 # VARIABLES FOR SETUP
-# make sure not to have trailing slashes.
-# gets current working directory
 # BASEDIR="$PWD"
+BASEDIR="$HOME/ambrosic/FluidX3D"
+
+# make sure not to have trailing slashes.
 INPUTFOLDER="$BASEDIR/bin/export"
 OUTFOLDERNAME="videoOutput"
 # everything below here shouldn't need tweaks...
 DIVIDER="=========================="
 # use date as a identifier so you don't have to rename all your mp4s all the time
+
 date=$(date '+%Y-%m-%d_%H_%M_%S')
 echo $date
 OUTPUTFOLDER="$BASEDIR/$OUTFOLDERNAME/$date"
+# echo $OUTPUTFOLDER
 mkdir -p "${OUTPUTFOLDER}"
-printf "RECURSIVE SUBDIR IN FOLDER: ${INPUTFOLDER} \nWITH OUTPUT TO:             ${OUTPUTFOLDER} \n$DIVIDER\nFolders to target:\n"
+echo "\nRECURSIVE SUBDIR IN FOLDER: ${INPUTFOLDER} \nWITH OUTPUT TO:             ${OUTPUTFOLDER} \n$DIVIDER\nFolders to target:\n"
 cd $INPUTFOLDER
 # print folders
 find . -maxdepth 1 -mindepth 1 -type d -printf '%P\n'
 printf "GENERATING MP4 FILES \n$DIVIDER\n"
 for D in $(find . -maxdepth 1 -mindepth 1 -type d -printf '%P\n'); do
-    echo "starting {D}"
-    cd {D}
+    echo "starting ${D}"
+    cd "${INPUTFOLDER}/${D}"
+    # ls
     ffmpeg -hide_banner -loglevel error -framerate 30 -pattern_type glob -i "*.png" -c:v libx264 -pix_fmt yuv420p "${D}.mp4"
-    echo "{D}.mp4 complete"
-    echo "moving {D}"
+    echo "${D}.mp4 complete, moving file to output directory"
+    # echo "moving ${D}"
     mv "$INPUTFOLDER/{D}/{D}.mp4" "$OUTPUTFOLDER/${D}.mp4"
+    echo "file moved"
     echo "$DIVIDER"
 done
 
